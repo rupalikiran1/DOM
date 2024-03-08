e1df0c5e2751f4de8297e01a057dc2e7024fb254
rupalikiran1 <rupalikiran1999@gmail.com>
2024-03-08 22:55:28 +0530

    added

---
+++ script.js
@@ -0,0 +1,18 @@
+// by tagName
+let q1 = document.querySelector('h2')
+console.log(q1)
+
+// by id
+let q2 = document.querySelector('#four')
+console.log(q2)
+
+// by class
+let q3 = document.querySelector('.five')
+console.log(q3)
+
+//byAttribute
+//tagName[attribute = "value"]
+
+let q4 = document.querySelector('h2[id="four]')
+console.log(q4)
+
