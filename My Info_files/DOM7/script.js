dab766c968967daff863b5500e157a4f4699d7d9
rupalikiran1 <rupalikiran1999@gmail.com>
2024-03-11 21:15:39 +0530

    added

---
+++ My Info_files/DOM7/script.js
@@ -0,0 +1,12 @@
+let ulList = document.querySelector('ul')
+console.log(ulList)
+let buttonA = document.querySelector('button')
+let inputText = document.querySelector('input')
+
+buttonA.addEventListener('click',function(){
+    let text = inputText.value // "papaya"
+    let newLi = document.createElement('li') //<li></li>
+    newLi.textContent = text //<li>papaya</li>
+    ulList.appendChild(newLi)
+    inputText.value = ""
+})
